5e337e8384ea347ff82c895d097f2d6e34636090
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-04-19 14:14:17 +0300

    Void and Never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -17,3 +17,26 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+// return edilmediği zamanlarda kullanılır.
+const Logger = (message: string): void => {
+  console.log(message);
+};
+
+// fonksiyonun hiçbir zaman hiçbir şey döndürmesini beklemediğimiz zaman never kullanılır.
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwSecondError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const throwThirdError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
