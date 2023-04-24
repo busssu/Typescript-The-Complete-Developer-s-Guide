22586d6c584e529bff73ae3d6dc1d7ae53cc2b0c
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-04-24 09:57:17 +0300

    Why Typed Arrays?

--- features/arrays.ts
+++ features/arrays.ts
@@ -2,3 +2,15 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 // const carsByMake = [['f150'], ['corolla'], ['camaro']];
 const carsByMake: string[][] = [];
+
+// Help with inference
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100); // Yanlnızca string eklenmesi gereken bir dizi olduğunu bize gösteriyor.
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
