b9d4fb8e80dd65fa2ae1d1476d89ea88ee651c68
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-04-25 15:50:45 +0300

    Exploring Type Definition Files

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,13 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
-import {} from 'google.maps';
+// import { User } from './User';
+// import { Company } from './Company';
+import {} from '@types/google.maps';
 
-const user = new User();
-
-console.log(user);
-
-const company = new Company();
-
-console.log(company);
-
-google;
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
