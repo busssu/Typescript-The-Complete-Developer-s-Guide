198551b8353098ac9471c5553c2bc277413a6490
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-04-26 10:54:53 +0300

    Why Use Private Modifiers?Here's Why

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,5 @@
 // import { User } from './User';
 // import { Company } from './Company';
-import {} from '@types/google.maps';
+import { CustomMapp } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMapp('map');
