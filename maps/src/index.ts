3bbe32bdf590a61816f79532d69a9ddb0cf0befd
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-04-26 14:22:39 +0300

    Duplicate Code

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,10 @@
 import { User } from './User';
-// import { Company } from './Company';
+import { Company } from './Company';
 import { CustomMapp } from './CustomMap';
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMapp('map');
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
