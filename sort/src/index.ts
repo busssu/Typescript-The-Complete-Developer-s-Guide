e4c7dedeb385700b83dd44f75d7320c7bf422572
Büşra Biner <87344103+busssu@users.noreply.github.com>
2023-05-22 11:13:53 +0300

    Typescript is Really Smart

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,22 +1,20 @@
 class Sorter {
-  // collection: number[];
-
-  // constructor(collection: number[]) {
-  //   this.collection = collection;
-  // }
-
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // If collection is an array of numbers
         if (this.collection[j] > this.collection[j + 1]) {
           const leftHand = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = leftHand;
         }
+
+        // If collection is a string, do this logic instead:
+        // ~~logic to compare and swap characters in a string
       }
     }
   }
